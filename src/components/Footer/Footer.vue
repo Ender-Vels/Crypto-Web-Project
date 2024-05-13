5efb85a8a6c7fb9c78ff43da04aa46195a3abf56
Ender-Vels <endervels1999@gmail.com>
2024-05-13 16:46:34 +0300

    HomePage

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -28,7 +28,128 @@
     </div>
 
     <div class="container col mt-5">
+      <ul class="list-unstyled">
+        <li>
+          <RouterLink class="fw-bold text-dark text-decoration-none fs-5" to="/"> Company </RouterLink>
 
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> About </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Careers </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Affiliates </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Press </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Investors </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Legal & Privacy </RouterLink>
+
+        </li>
+      </ul>
+    </div>
+
+    <div class="container col mt-5">
+      <ul class="list-unstyled">
+        <li>
+          <RouterLink class="fw-bold text-dark text-decoration-none fs-5" to="/"> Features </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Buy & Sell Crypto </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Wallet </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Card </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Dashboard </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Trading </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Portfolio </RouterLink>
+
+        </li>
+      </ul>
+    </div>
+
+    <div class="container col mt-5">
+      <ul class="list-unstyled">
+        <li>
+          <RouterLink class="fw-bold text-dark text-decoration-none fs-5" to="/"> Developers </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Cloud</RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Wallet SDK </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Query & Transact </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Commerce </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Exchange & Pro </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> API Access </RouterLink>
+
+        </li>
+      </ul>
+    </div>
+
+    <div class="container col mt-5">
+      <ul class="list-unstyled">
+        <li>
+          <RouterLink class="fw-bold text-dark text-decoration-none fs-5" to="/"> Resources </RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Blog</RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> Help & Support</RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/">Customer Stories</RouterLink>
+
+        </li>
+        <li>
+          <RouterLink class="text-dark text-decoration-none" to="/"> FAQ </RouterLink>
+
+        </li>
+
+      </ul>
     </div>
 
 
